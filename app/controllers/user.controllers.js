bcc11106e610487d2deec28c3a660304654547a7
mehrdadsadeghi228 <63063456+mehrdadsadeghi228@users.noreply.github.com>
2023-08-20 14:08:46 -0700

    Update user.controllers.js

--- app/controllers/user.controllers.js
+++ app/controllers/user.controllers.js
@@ -1,7 +1,9 @@
 const Controller = require("./controller");
 const createError = require("http-errors");
 const {StatusCodes: HttpStatus} = require("http-status-codes");
-
+const bcrypt=require('bcrypt');
+const { CheckExistUser } = require("../utils/utils");
+const { UserModel } = require("../models/user.model");
 class UserController extends Controller{
 
     UserIndex(res,req,next) {
@@ -10,7 +12,24 @@ class UserController extends Controller{
             message:'index page User'
         });
     }
+    async signupUser(res,req){
+        const {password , email , userName , lastName , mobile , name }=req.body
+        CheckExistUser(userName);
 
+        const genSalt= bcrypt.genSaltSync(process.env.SALT)
+        const newPass= bcrypt.hashSync(password,genSalt)
+        const newUser= await UserModel.create({
+            userName,name,lastName,mobile,password:newPass,email
+        });
+
+        res.statusCodes(HttpStatus.Ok).json({
+            statusCodes:200,
+            message:newUser
+        })
+
+
+
+    }
 
 
 }
